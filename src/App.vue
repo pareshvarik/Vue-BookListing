1fd66a5a6fcb8e9cc6ba113e9f1874188ad09d87
pareshvarik <varikparesh97@gmail.com>
2020-12-27 12:21:37 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <hello-world></hello-world> -->
+    <book-list>
+  </div>
+</template>
+
+<script>
+import BookList from './components/BookList';
+
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'app',
+  components: {
+    BookList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
